b922cf9562d8c93443fad44e39836c92287b4564
umoeller <umoeller@ad77b7e9-edad-e311-93b4-62de69f6b5d0>
2000-03-04 19:31:47 +0000

    Updated documentation.
    git-svn-id: http://svn.netlabs.org/repos/xworkplace/trunk@17 ad77b7e9-edad-e311-93b4-62de69f6b5d0

--- 001/misc/crobj001.cmd
+++ 001/misc/crobj001.cmd
@@ -38,6 +38,9 @@ ShowAllInTreeView   = "Show all objects in Tree view";
 WorkplaceShell      = "Workplace Shell";
 XWPSetup            = "XWorkplace Setup";
 OS2Kernel           = "OS/2 Kernel";
+Screen              = "Screen";
+Mouse               = "Mouse";
+Keyboard            = "Keyboard";
 Treesize            = "Treesize";
 PackTree            = "Pack this tree";
 
@@ -52,51 +55,57 @@ pdir = left(dir, length(dir)-8);
 idir = dir;
 dir = pdir||"bin\";
 
-rc = SysCreateObject("WPFolder", ConfigFolder, "<XFOLDER_MAIN>", "ICONVIEW=NONFLOWED,MINI;DEFAULTVIEW=ICON;SHOWALLINTREEVIEW=YES;ALWAYSSORT=NO;OBJECTID=<XFOLDER_CONFIG>", "U");
+rc = SysCreateObject("WPFolder", ConfigFolder, "<XWP_MAINFLDR>", "ICONVIEW=NONFLOWED,MINI;DEFAULTVIEW=ICON;SHOWALLINTREEVIEW=YES;ALWAYSSORT=NO;OBJECTID=<XWP_CONFIG>", "U");
 if (\rc) then
-    rc = SysCreateObject("WPFolder", ConfigFolder, "<WP_DESKTOP>", "ICONVIEW=NONFLOWED,MINI;DEFAULTVIEW=ICON;SHOWALLINTREEVIEW=YES;ALWAYSSORT=NO;OBJECTID=<XFOLDER_CONFIG>", "U");
+    rc = SysCreateObject("WPFolder", ConfigFolder, "<WP_DESKTOP>", "ICONVIEW=NONFLOWED,MINI;DEFAULTVIEW=ICON;SHOWALLINTREEVIEW=YES;ALWAYSSORT=NO;OBJECTID=<XWP_CONFIG>", "U");
 
 /* create config folders */
-rc = SysCreateObject("WPFolder", CommandLines, "<XFOLDER_CONFIG>", "DEFAULTVIEW=ICON;ICONVIEW=NONFLOWED,MINI;SHOWALLINTREEVIEW=YES;ALWAYSSORT=NO;OBJECTID=<XFOLDER_CFG1>", "U");
+rc = SysCreateObject("WPFolder", CommandLines, "<XWP_CONFIG>", "DEFAULTVIEW=ICON;ICONVIEW=NONFLOWED,MINI;SHOWALLINTREEVIEW=YES;ALWAYSSORT=NO;OBJECTID=<XWP_CFG1>", "U");
     if (SysSearchPath("PATH", "CMDSHL.CMD") \= "") then
-        rc = SysCreateObject("WPProgram", OS2Win||" (CmdShl)", "<XFOLDER_CFG1>", "EXENAME=cmdshl.cmd;PARAMETERS=%;PROGTYPE=WINDOWABLEVIO;CCVIEW=YES;OBJECTID=<XFOLDER_CMDSHL>;", "R");
+        rc = SysCreateObject("WPProgram", OS2Win||" (CmdShl)", "<XWP_CFG1>", "EXENAME=cmdshl.cmd;PARAMETERS=%;PROGTYPE=WINDOWABLEVIO;CCVIEW=YES;OBJECTID=<XWP_CMDSHL>;", "R");
     if (SysSearchPath("PATH", "BASH.EXE") \= "") then
-        rc = SysCreateObject("WPProgram", OS2Win||" (bash)", "<XFOLDER_CFG1>", "EXENAME=bash.exe;PARAMETERS=%;PROGTYPE=WINDOWABLEVIO;CCVIEW=YES;OBJECTID=<XFOLDER_BASH>;", "R");
+        rc = SysCreateObject("WPProgram", OS2Win||" (bash)", "<XWP_CFG1>", "EXENAME=bash.exe;PARAMETERS=%;PROGTYPE=WINDOWABLEVIO;CCVIEW=YES;OBJECTID=<XWP_BASH>;", "R");
     if (SysSearchPath("PATH", "KSH.EXE") \= "") then
-        rc = SysCreateObject("WPProgram", OS2Win||" (ksh)", "<XFOLDER_CFG1>", "EXENAME=ksh.exe;PARAMETERS=%;PROGTYPE=WINDOWABLEVIO;CCVIEW=YES;OBJECTID=<XFOLDER_KSH>;", "R");
-    rc = SysCreateObject("WPProgram", OS2Win, "<XFOLDER_CFG1>", "EXENAME=*;PARAMETERS=%;PROGTYPE=WINDOWABLEVIO;CCVIEW=YES;OBJECTID=<XFOLDER_OS2WIN>;", "R");
-    rc = SysCreateObject("WPProgram", OS2Fullscreen, "<XFOLDER_CFG1>", "EXENAME=*;PARAMETERS=%;PROGTYPE=FULLSCREEN;CCVIEW=YES;OBJECTID=<XFOLDER_OS2FULL>;", "R");
-    rc = SysCreateObject("WPProgram", DosWin, "<XFOLDER_CFG1>", "EXENAME=*;PARAMETERS=%;PROGTYPE=WINDOWEDVDM;CCVIEW=YES;OBJECTID=<XFOLDER_DOSWIN>;", "R");
-    rc = SysCreateObject("WPProgram", DosFullscreen, "<XFOLDER_CFG1>", "EXENAME=*;PARAMETERS=%;PROGTYPE=VDM;CCVIEW=YES;OBJECTID=<XFOLDER_DOSFULL>;", "R");
+        rc = SysCreateObject("WPProgram", OS2Win||" (ksh)", "<XWP_CFG1>", "EXENAME=ksh.exe;PARAMETERS=%;PROGTYPE=WINDOWABLEVIO;CCVIEW=YES;OBJECTID=<XWP_KSH>;", "R");
+    rc = SysCreateObject("WPProgram", OS2Win, "<XWP_CFG1>", "EXENAME=*;PARAMETERS=%;PROGTYPE=WINDOWABLEVIO;CCVIEW=YES;OBJECTID=<XWP_OS2WIN>;", "R");
+    rc = SysCreateObject("WPProgram", OS2Fullscreen, "<XWP_CFG1>", "EXENAME=*;PARAMETERS=%;PROGTYPE=FULLSCREEN;CCVIEW=YES;OBJECTID=<XWP_OS2FULL>;", "R");
+    rc = SysCreateObject("WPProgram", DosWin, "<XWP_CFG1>", "EXENAME=*;PARAMETERS=%;PROGTYPE=WINDOWEDVDM;CCVIEW=YES;OBJECTID=<XWP_DOSWIN>;", "R");
+    rc = SysCreateObject("WPProgram", DosFullscreen, "<XWP_CFG1>", "EXENAME=*;PARAMETERS=%;PROGTYPE=VDM;CCVIEW=YES;OBJECTID=<XWP_DOSFULL>;", "R");
 
-rc = SysCreateObject("WPFolder", CreateAnother, "<XFOLDER_CONFIG>", "DEFAULTVIEW=ICON;ICONVIEW=NONFLOWED,MINI;SHOWALLINTREEVIEW=YES;ALWAYSSORT=NO;OBJECTID=<XFOLDER_CFG2>", "U");
-    rc = SysCreateObject("WPFolder", Folder, "<XFOLDER_CFG2>", "TEMPLATE=YES;OBJECTID=<XFOLDER_FOLDERTEMPLATE>;", "R");
-    rc = SysCreateObject("WPProgram", ProgramObject, "<XFOLDER_CFG2>", "TEMPLATE=YES;OBJECTID=<XFOLDER_PROGRAMTEMPLATE>;", "R");
+rc = SysCreateObject("WPFolder", CreateAnother, "<XWP_CONFIG>", "DEFAULTVIEW=ICON;ICONVIEW=NONFLOWED,MINI;SHOWALLINTREEVIEW=YES;ALWAYSSORT=NO;OBJECTID=<XWP_CFG2>", "U");
+    rc = SysCreateObject("WPFolder", Folder, "<XWP_CFG2>", "TEMPLATE=YES;OBJECTID=<XWP_FOLDERTEMPLATE>;", "R");
+    rc = SysCreateObject("WPProgram", ProgramObject, "<XWP_CFG2>", "TEMPLATE=YES;OBJECTID=<XWP_PROGRAMTEMPLATE>;", "R");
 
-rc = SysCreateObject("WPFolder", QuickSettings, "<XFOLDER_CONFIG>", "DEFAULTVIEW=ICON;ICONVIEW=NONFLOWED,MINI;SHOWALLINTREEVIEW=YES;ALWAYSSORT=NO;OBJECTID=<XFOLDER_CFG3>", "U");
-    rc = SysCreateObject("WPFolder", DefaultView, "<XFOLDER_CFG3>", "DEFAULTVIEW=ICON;ICONVIEW=NONFLOWED,MINI;SHOWALLINTREEVIEW=YES;ALWAYSSORT=NO;OBJECTID=<XFOLDER_CFG3_1>", "U");
-        rc = SysCreateObject("WPProgram", IconView, "<XFOLDER_CFG3_1>", "EXENAME="dir'deficon.cmd;CCVIEW=YES;MINIMIZED=YES;NOAUTOCLOSE=NO;OBJECTID=<XFOLDER_DEFICON>;', "R");
-        rc = SysCreateObject("WPProgram", TreeView, "<XFOLDER_CFG3_1>", "EXENAME="dir'deftree.cmd;CCVIEW=YES;MINIMIZED=YES;NOAUTOCLOSE=NO;OBJECTID=<XFOLDER_DEFTREE>;', "R");
-        rc = SysCreateObject("WPProgram", DetailsView, "<XFOLDER_CFG3_1>", "EXENAME="dir'defdetls.cmd;CCVIEW=YES;MINIMIZED=YES;NOAUTOCLOSE=NO;OBJECTID=<XFOLDER_DEFDETLS>;', "R");
-    rc = SysCreateObject("WPProgram", SmallIcons, "<XFOLDER_CFG3>", "EXENAME="dir'icosmall.cmd;CCVIEW=YES;MINIMIZED=YES;NOAUTOCLOSE=NO;OBJECTID=<XFOLDER_ICOSMALL>;', "R");
-    rc = SysCreateObject("WPProgram", NormalIcons, "<XFOLDER_CFG3>", "EXENAME="dir'iconorm.cmd;CCVIEW=YES;MINIMIZED=YES;NOAUTOCLOSE=NO;OBJECTID=<XFOLDER_ICONORM>;', "R");
-    rc = SysCreateObject("WPProgram", ShowAllInTreeView, "<XFOLDER_CFG3>", "EXENAME="dir'showall.cmd;CCVIEW=YES;MINIMIZED=YES;NOAUTOCLOSE=NO;OBJECTID=<XFOLDER_SHOWALL>;', "R");
+rc = SysCreateObject("WPFolder", QuickSettings, "<XWP_CONFIG>", "DEFAULTVIEW=ICON;ICONVIEW=NONFLOWED,MINI;SHOWALLINTREEVIEW=YES;ALWAYSSORT=NO;OBJECTID=<XWP_CFG3>", "U");
+    rc = SysCreateObject("WPFolder", DefaultView, "<XWP_CFG3>", "DEFAULTVIEW=ICON;ICONVIEW=NONFLOWED,MINI;SHOWALLINTREEVIEW=YES;ALWAYSSORT=NO;OBJECTID=<XWP_CFG3_1>", "U");
+        rc = SysCreateObject("WPProgram", IconView, "<XWP_CFG3_1>", "EXENAME="dir'deficon.cmd;CCVIEW=YES;MINIMIZED=YES;NOAUTOCLOSE=NO;OBJECTID=<XWP_DEFICON>;', "R");
+        rc = SysCreateObject("WPProgram", TreeView, "<XWP_CFG3_1>", "EXENAME="dir'deftree.cmd;CCVIEW=YES;MINIMIZED=YES;NOAUTOCLOSE=NO;OBJECTID=<XWP_DEFTREE>;', "R");
+        rc = SysCreateObject("WPProgram", DetailsView, "<XWP_CFG3_1>", "EXENAME="dir'defdetls.cmd;CCVIEW=YES;MINIMIZED=YES;NOAUTOCLOSE=NO;OBJECTID=<XWP_DEFDETLS>;', "R");
+    rc = SysCreateObject("WPProgram", SmallIcons, "<XWP_CFG3>", "EXENAME="dir'icosmall.cmd;CCVIEW=YES;MINIMIZED=YES;NOAUTOCLOSE=NO;OBJECTID=<XWP_ICOSMALL>;', "R");
+    rc = SysCreateObject("WPProgram", NormalIcons, "<XWP_CFG3>", "EXENAME="dir'iconorm.cmd;CCVIEW=YES;MINIMIZED=YES;NOAUTOCLOSE=NO;OBJECTID=<XWP_ICONORM>;', "R");
+    rc = SysCreateObject("WPProgram", ShowAllInTreeView, "<XWP_CFG3>", "EXENAME="dir'showall.cmd;CCVIEW=YES;MINIMIZED=YES;NOAUTOCLOSE=NO;OBJECTID=<XWP_SHOWALL>;', "R");
+
+rc = SysCreateObject("WPFolder", "~XWorkplace", "<XWP_CONFIG>", "DEFAULTVIEW=ICON;ICONVIEW=NONFLOWED,MINI;SHOWALLINTREEVIEW=YES;ALWAYSSORT=NO;OBJECTID=<XWP_CFG4>", "U");
+    rc = SysCreateObject("WPShadow", XWPSetup, "<XWP_CFG4>", "SHADOWID=<XWORKPLACE_SETUP>;OBJECTID=<XWORKPLACE_SETUPCFGSHADOW>;", "R");
+    rc = SysCreateObject("WPShadow", WorkplaceShell, "<XWP_CFG4>", "SHADOWID=<XWP_WPS>;OBJECTID=<XWP_WPSCFGSHADOW>;", "R");
+    rc = SysCreateObject("WPShadow", Mouse, "<XWP_CFG4>", "SHADOWID=<WP_MOUSE>;OBJECTID=<XWP_WPMOUSECFGSHADOW>;", "R");
+    rc = SysCreateObject("WPShadow", Keyboard, "<XWP_CFG4>", "SHADOWID=<WP_KEYB>;OBJECTID=<XWP_WPKEYBCFGSHADOW>;", "R");
+    rc = SysCreateObject("WPShadow", Screen, "<XWP_CFG4>", "SHADOWID=<XWP_SCREEN>;OBJECTID=<XWP_SCREENCFGSHADOW>;", "R");
+    rc = SysCreateObject("WPShadow", "OS/2 Kernel", "<XWP_CFG4>", "SHADOWID=<XWP_KERNEL>;OBJECTID=<XWP_KERNELCFGSHADOW>;", "R");
+    rc = SysCreateObject("WPProgram", "---", "<XWP_CFG4>", "OBJECTID=<XWP_SEP41>;", "R");
+    rc = SysCreateObject("WPShadow", "User Guide", "<XWP_CFG4>", "SHADOWID=<XWP_REF>;OBJECTID=<XWP_REFCFGSHADOW>;", "R");
+    rc = SysCreateObject("WPProgram", "---", "<XWP_CFG4>", "OBJECTID=<XWP_SEP42>;", "R");
+    rc = SysCreateObject("WPShadow", ConfigFolder, "<XWP_CFG4>", "SHADOWID=<XWP_CONFIG>;OBJECTID=<XWP_CONFIGCFGSHADOW>;", "R");
+
+rc = SysCreateObject("WPProgram", "---", "<XWP_CONFIG>", "OBJECTID=<XWP_SEP1>;", "R");
 
 if (SysSearchPath("PATH", "NETSCAPE.EXE") \= "") then
-    rc = SysCreateObject("WPProgram", "Netscape (DDE)", "<XFOLDER_CONFIG>", "EXENAME="dir"netscdde.exe;OBJECTID=<XFOLDER_NETSCAPE>;", "R");
+    rc = SysCreateObject("WPProgram", "Netscape (DDE)", "<XWP_CONFIG>", "EXENAME="dir"netscdde.exe;OBJECTID=<XWP_NETSCAPE>;", "R");
 
-rc = SysCreateObject("WPProgram", Treesize, "<XFOLDER_CONFIG>", "EXENAME="dir"treesize.exe;CCVIEW=YES;OBJECTID=<XFOLDER_TREESIZE>;", "R");
+rc = SysCreateObject("WPProgram", Treesize, "<XWP_CONFIG>", "EXENAME="dir"treesize.exe;CCVIEW=YES;OBJECTID=<XWP_TREESIZE>;", "R");
 
 if (SysSearchPath("PATH", "ZIP.EXE") \= "") then
-    rc = SysCreateObject("WPProgram", PackTree, "<XFOLDER_CONFIG>", "EXENAME="dir"packtree.cmd;CCVIEW=YES;OBJECTID=<XFOLDER_PACKTREE>;", "R");
+    rc = SysCreateObject("WPProgram", PackTree, "<XWP_CONFIG>", "EXENAME="dir"packtree.cmd;CCVIEW=YES;OBJECTID=<XWP_PACKTREE>;", "R");
 
-rc = SysCreateObject("WPFolder", "~XWorkplace", "<XFOLDER_CONFIG>", "DEFAULTVIEW=ICON;ICONVIEW=NONFLOWED,MINI;SHOWALLINTREEVIEW=YES;ALWAYSSORT=NO;OBJECTID=<XFOLDER_CFG4>", "U");
-    rc = SysCreateObject("WPShadow", "Online Reference", "<XFOLDER_CFG4>", "SHADOWID=<XFOLDER_REF>;OBJECTID=<XFOLDER_REFSHADOW>;", "R");
-    rc = SysCreateObject("WPShadow", ConfigFolder, "<XFOLDER_CFG4>", "SHADOWID=<XFOLDER_CONFIG>;OBJECTID=<XFOLDER_CONFIGSHADOW>;", "R");
-    rc = SysCreateObject("WPShadow", WorkplaceShell, "<XFOLDER_CFG4>", "SHADOWID=<XFOLDER_WPS>;OBJECTID=<XFOLDER_WPSCFGSHADOW>;", "R");
-    rc = SysCreateObject("WPShadow", "OS/2 Kernel", "<XFOLDER_CFG4>", "SHADOWID=<XFOLDER_KERNEL>;OBJECTID=<XFOLDER_KERNELCFGSHADOW>;", "R");
-    rc = SysCreateObject("WPShadow", XWPSetup, "<XFOLDER_CFG4>", "SHADOWID=<XWORKPLACE_SETUP>;OBJECTID=<XWORKPLACE_SETUPCFGSHADOW>;", "R");
-
-rc = SysCreateObject("WPProgram", "---", "<XFOLDER_CONFIG>", "OBJECTID=<XFOLDER_SEP1>;", "R");
 
 
--- 001/misc/instl001.cmd
+++ 001/misc/instl001.cmd
@@ -5,7 +5,7 @@
  */
 
 /*
-    This file gets called by WarpIN after installation was successful.
+    This file gets called by WarpIN after all files have been unpacked.
     This calls CROBJxxx.CMD in turn to have the config folder created.
 */
 
@@ -25,7 +25,12 @@ OnlineReference     = "XWorkplace User Guide";
 /* other objects */
 WorkplaceShell      = "Workplace Shell";
 XWPSetup            = "XWorkplace Setup";
+XWPClassList        = "WPS Class List";
 OS2Kernel           = "OS/2 Kernel";
+Screen              = "Screen";
+Mouse               = "Mouse";
+Keyboard            = "Keyboard";
+Sound               = "Sound";
 XShutdown           = "eXtended Shutdown...";
 
 /* DO NOT CHANGE the following */
@@ -42,30 +47,55 @@ dir = pdir||"bin\";
 OnlineReferenceFile = "xfldr"LanguageCode".inf";
 XFolderIntroFile    = "xfldr"LanguageCode".sgs";
 
-rc = SysCreateObject("WPFolder", XFolderMain, "<WP_DESKTOP>", "DEFAULTVIEW=ICON;SHOWALLINTREEVIEW=YES;ALWAYSSORT=NO;OBJECTID=<XFOLDER_MAIN>", "U");
+rc = SysCreateObject("WPFolder", XFolderMain, "<WP_DESKTOP>", "DEFAULTVIEW=ICON;SHOWALLINTREEVIEW=YES;ALWAYSSORT=NO;OBJECTID=<XWP_MAINFLDR>", "R");
 if (SysSearchPath("PATH", "sguide.exe") \= "") then
-    rc = SysCreateObject("WPProgram", XFolderIntro, "<XFOLDER_MAIN>", "EXENAME=sguide.exe;PARAMETERS="XFolderIntroFile";STARTUPDIR="dir";ICONFILE="idir"xfolder.ico;OBJECTID=<XFOLDER_INTRO>", "U");
-rc = SysCreateObject("WPProgram", OnlineReference, "<XFOLDER_MAIN>", "EXENAME=view.exe;PARAMETERS="pdir||OnlineReferenceFile";OBJECTID=<XFOLDER_REF>", "U");
-rc = SysCreateObject("WPProgram", XShutdown, "<XFOLDER_MAIN>", "EXENAME="dir"xshutdwn.exe;OBJECTID=<XFOLDER_XSHUTDOWN>", "U");
+    rc = SysCreateObject("WPProgram", XFolderIntro, "<XWP_MAINFLDR>", "EXENAME=sguide.exe;PARAMETERS="XFolderIntroFile";STARTUPDIR="dir";ICONFILE="idir"xfolder.ico;OBJECTID=<XWP_INTRO>", "R");
+/* "Readme" shadow (added V0.9.2) */
+rc = SysCreateObject("WPShadow", XWPSetup, "<XWP_MAINFLDR>", "SHADOWID="pdir||"README;OBJECTID=<XWP_READMEMAINSHADOW>;", "R");
 
-/* create "XWorkplace Setup" (added V1.00) */
-rc = SysCreateObject("XWPSetup", XWPSetup, "<WP_CONFIG>", "OBJECTID=<XWORKPLACE_SETUP>;", "U");
+/* User Guide */
+rc = SysCreateObject("WPProgram", OnlineReference, "<XWP_MAINFLDR>", "EXENAME=view.exe;PARAMETERS="pdir||OnlineReferenceFile";OBJECTID=<XWP_REF>", "R");
+
+/* XSHUTDWN.EXE */
+rc = SysCreateObject("WPProgram", XShutdown, "<XWP_MAINFLDR>", "EXENAME="dir"xshutdwn.exe;OBJECTID=<XWP_XSHUTDOWN>", "R");
+
+/* create "XWorkplace Setup" (added V0.9.0) */
+rc = SysCreateObject("XWPSetup", XWPSetup, "<WP_CONFIG>", "OBJECTID=<XWORKPLACE_SETUP>;", "R");
 if (\rc) then
-    rc = SysCreateObject("XWPSetup", XWPSetup, "<WP_DESKTOP>", "OBJECTID=<XWORKPLACE_SETUP>;", "U");
-rc = SysCreateObject("WPShadow", XWPSetup, "<XFOLDER_MAIN>", "SHADOWID=<XWORKPLACE_SETUP>;OBJECTID=<XWORKPLACE_SETUPSHADOW>;", "U");
+    rc = SysCreateObject("XWPSetup", XWPSetup, "<WP_DESKTOP>", "OBJECTID=<XWORKPLACE_SETUP>;", "R");
+rc = SysCreateObject("WPShadow", XWPSetup, "<XWP_MAINFLDR>", "SHADOWID=<XWORKPLACE_SETUP>;OBJECTID=<XWP_SETUPMAINSHADOW>;", "R");
+
+/* create "WPS Class List" (added V0.9.2) */
+rc = SysCreateObject("XWPClassList", XWPClassList, "<WP_CONFIG>", "OBJECTID=<XWP_CLASSLIST>;", "R");
+if (\rc) then
+    rc = SysCreateObject("XWPClassList", XWPClassList, "<WP_DESKTOP>", "OBJECTID=<XWP_CLASSLIST>;", "R");
+rc = SysCreateObject("WPShadow", XWPClassList, "<XWP_MAINFLDR>", "SHADOWID=<XWP_CLASSLIST>;OBJECTID=<XWP_CLASSLISTMAINSHADOW>;", "R");
 
 /* create "OS/2 Kernel" */
-rc = SysCreateObject("XFldSystem", OS2Kernel, "<WP_CONFIG>", "OBJECTID=<XFOLDER_KERNEL>;", "U");
+rc = SysCreateObject("XFldSystem", OS2Kernel, "<WP_CONFIG>", "OBJECTID=<XWP_KERNEL>;", "R");
 if (\rc) then
-    /* fixed with V0.85: changed following <XFOLDER_WPS> to <XFOLDER_KERNEL> */
-    rc = SysCreateObject("XFldSystem", OS2Kernel, "<WP_DESKTOP>", "OBJECTID=<XFOLDER_KERNEL>;", "U");
-rc = SysCreateObject("WPShadow", OS2Kernel, "<XFOLDER_MAIN>", "SHADOWID=<XFOLDER_KERNEL>;OBJECTID=<XFOLDER_KERNELSHADOW>;", "U");
+    /* fixed with V0.85: changed following <XWP_WPS> to <XWP_KERNEL> */
+    rc = SysCreateObject("XFldSystem", OS2Kernel, "<WP_DESKTOP>", "OBJECTID=<XWP_KERNEL>;", "R");
+rc = SysCreateObject("WPShadow", OS2Kernel, "<XWP_MAINFLDR>", "SHADOWID=<XWP_KERNEL>;OBJECTID=<XWP_KERNELMAINSHADOW>;", "R");
 
 /* create "Workplace Shell" */
-rc = SysCreateObject("XFldWPS", WorkplaceShell, "<WP_CONFIG>", "OBJECTID=<XFOLDER_WPS>", "U");
+rc = SysCreateObject("XFldWPS", WorkplaceShell, "<WP_CONFIG>", "OBJECTID=<XWP_WPS>", "R");
 if (\rc) then
-    rc = SysCreateObject("XFldWPS", WorkplaceShell, "<WP_DESKTOP>", "OBJECTID=<XFOLDER_WPS>", "U");
-rc = SysCreateObject("WPShadow", WorkplaceShell, "<XFOLDER_MAIN>", "SHADOWID=<XFOLDER_WPS>;OBJECTID=<XFOLDER_WPSSHADOW>;", "U");
+    rc = SysCreateObject("XFldWPS", WorkplaceShell, "<WP_DESKTOP>", "OBJECTID=<XWP_WPS>", "R");
+rc = SysCreateObject("WPShadow", WorkplaceShell, "<XWP_MAINFLDR>", "SHADOWID=<XWP_WPS>;OBJECTID=<XWP_WPSMAINSHADOW>;", "R");
+
+/* create "Screen" (added V0.9.2) */
+rc = SysCreateObject("XWPScreen", Screen, "<WP_CONFIG>", "OBJECTID=<XWP_SCREEN>", "R");
+if (\rc) then
+    rc = SysCreateObject("XWPScreen", Screen, "<WP_DESKTOP>", "OBJECTID=<XWP_SCREEN>", "R");
+rc = SysCreateObject("WPShadow", Screen, "<XWP_MAINFLDR>", "SHADOWID=<XWP_SCREEN>;OBJECTID=<XWP_SCREENMAINSHADOW>;", "R");
+
+/* "Mouse" shadow (added V0.9.2) */
+rc = SysCreateObject("WPShadow", Mouse, "<XWP_MAINFLDR>", "SHADOWID=<WP_MOUSE>;OBJECTID=<XWP_MOUSEMAINSHADOW>;", "R");
+/* "Keyboard" shadow (added V0.9.2) */
+rc = SysCreateObject("WPShadow", Keyboard, "<XWP_MAINFLDR>", "SHADOWID=<WP_KEYB>;OBJECTID=<XWP_KEYBMAINSHADOW>;", "R");
+/* "Sound" shadow (added V0.9.2) */
+rc = SysCreateObject("WPShadow", Sound, "<XWP_MAINFLDR>", "SHADOWID=<WP_SOUND>;OBJECTID=<XWP_SOUNDMAINSHADOW>;", "R");
 
 "@call "idir"crobj"LanguageCode
 
