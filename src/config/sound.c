115c7c1aab1d8ebe0be4c619678bb2a184ad7dde
pr <pr@ad77b7e9-edad-e311-93b4-62de69f6b5d0>
2005-10-17 19:03:11 +0000

    Bart's fixes for bug 31
    git-svn-id: http://svn.netlabs.org/repos/xworkplace/trunk@726 ad77b7e9-edad-e311-93b4-62de69f6b5d0

--- src/config/sound.c
+++ src/config/sound.c
@@ -21,7 +21,7 @@
  */
 
 /*
- *      Copyright (C) 1997-2003 Ulrich M”ller.
+ *      Copyright (C) 1997-2005 Ulrich M”ller.
  *
  *      This file is part of the XWorkplace source package.
  *      XWorkplace is free software; you can redistribute it and/or modify
